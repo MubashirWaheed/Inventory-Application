df77b474eef11b0f2bdd46e6b576030dd4dcdb11
MubashirWaheed <mubashir3080@gmail.com>
2021-12-18 13:17:03 +0500

    Fixed bug

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -11,32 +11,38 @@ const URI = process.env.MONGODB;
 
 // Diplays Category page
 exports.categories = (req, res)=>{
-    res.render('categories');
-} 
+    Categories.find({}, 'name description')
+    .exec(function(err, items){
+        res.render('categories', {items: items});
+    })
+}
+
 // Displays Create category form
 exports.categories_create_get = (req, res)=>{
     res.render('form');
 }
+
 // Date is sent to server on form submission 
 exports.categories_create_post = [
     // Data sanitization
-    body('name').trim().isLength({min:1}).escape().withMessage('Name must be specified.'),
-    
-    // Error handling 
-    (req, res,next)=>{
+    body('categoryName').trim().isLength({min:1}).escape().withMessage('Name must be specified.'),
+    body('categoryDescription').trim().isLength({min:1}).escape().withMessage('Name must be specified.'),
+
+    (req, res, next)=>{
+        // Error handling 
         const errors = validationResult(req);
         if(!errors.isEmpty()){
+            // Pass that error to form 
             res.render('form');
             return;
         }else{
-            console.log(req.body);
             const category = new Categories({
                 name:req.body.categoryName,
                 description:req.body.categoryDescription
             });
             category.save(function(err){
                 if (err) return  next(err)
-                res.redirect('/');
+                res.redirect('categories');
             })
         }
     }
